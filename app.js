eb08533871524eed42c538d762b056dc9e775a94
ssarun9871 <ssarun9871@gmail.com>
2026-01-06 18:30:06 +0530

    publishing and consuming messages

--- app.js
+++ app.js
@@ -24,16 +24,26 @@ const send = async (to, subject, content) => {
 
 }
 
-async function connectRabbitMQ() {
+
+async function messagingQueue() {
     try {
         const connection = await amqplib.connect("amqp://localhost");
         const channel = await connection.createChannel();
         await channel.assertQueue("testing123");//it will create a new queue named 'testing123'
         console.log("channel created")
-    }
 
+        //publishing messages
+        setInterval(() => {
+            channel.sendToQueue("testing123", Buffer.from("this is the msg"));
+        }, 100)
+
+        //consuming messages
+        await channel.consume("testing123", (data) => {
+            console.log(`${Buffer.from(data.content)}`);
+        })
+    }
     catch (err) {
-        console.error("Unable to establish connection with RabbitMQ : " + err);
+        console.error("Unable to establish connection : " + err);
     }
 }
 
@@ -42,6 +52,6 @@ app.use(express.urlencoded({ extended: false, limit: '1mb' }));
 
 app.listen(PORT, () => {
     console.log('Server is live on port ' + PORT);
-    connectRabbitMQ();
+    messagingQueue();
     send('yo.aksinha@gmail.com', 'nodemailer testing', 'hi');
 })
